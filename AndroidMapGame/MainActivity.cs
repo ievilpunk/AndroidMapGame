e7c3f867080cb4c3e17e46acee7f9aba6450c44a
scratch <ievilpunk@gmail.com>
2017-12-08 21:32:24 -0500

    tweaked camera controls and set location based on gps (untested)

--- AndroidMapGame/MainActivity.cs
+++ AndroidMapGame/MainActivity.cs
@@ -1,19 +1,106 @@
 ﻿using Android.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
 
 namespace AndroidMapGame
 {
     [Activity(Label = "AndroidMapGame", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, IOnMapReadyCallback, ILocationListener
     {
+        private static LatLng current = new LatLng(42.6382576, -73.73495911);
+        private static CameraUpdate cameraUpdate;
+        private static int zoomLevel = 17;
+        private static int tiltLevel = 45;
+        private GoogleMap _map;
+        private MapFragment _mapFragment;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            MapInit();
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            SetupMapIfNeeded();
+        }
+
+        private void MapInit()
+        {
+            _mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+            if (_mapFragment == null)
+            {
+                GoogleMapOptions mapOptions = new GoogleMapOptions()
+                    .InvokeMapType(GoogleMap.MapTypeNormal)
+                    .InvokeZoomControlsEnabled(false)
+                    .InvokeCompassEnabled(true);
+
+                FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+                _mapFragment = MapFragment.NewInstance(mapOptions);
+                fragTx.Add(Resource.Id.map, _mapFragment, "map");
+                fragTx.Commit();
+            }
+            _mapFragment.GetMapAsync(this);
+        }
+
+        private void SetupMapIfNeeded()
+        {
+            if (_map != null)
+            {
+                MarkerOptions markerOpt1 = new MarkerOptions();
+                markerOpt1.SetPosition(current);
+                markerOpt1.SetTitle("Home");
+                markerOpt1.SetIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueCyan));
+                _map.AddMarker(markerOpt1);
+
+                // We create an instance of CameraUpdate, and move the map to it.\
+                CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+                builder.Target(current);
+                builder.Tilt(tiltLevel);
+                builder.Zoom(zoomLevel);
+                builder.Bearing(155);
+                CameraPosition cameraPosition = builder.Build();
+                cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+
+
+                //cameraUpdate = CameraUpdateFactory.NewLatLngZoom(current, zoomLevel);
+
+                _map.MoveCamera(cameraUpdate);
+            }
+        }
+
+        public void OnLocationChanged(Android.Locations.Location location)
+        {
+             current = new LatLng(location.Latitude, location.Longitude);
+             cameraUpdate = CameraUpdateFactory.NewLatLngZoom(current, zoomLevel);
+             _map.MoveCamera(cameraUpdate);
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            //Log.Debug(tag, provider + " disabled by user");
+        }
+        public void OnProviderEnabled(string provider)
+        {
+            //Log.Debug(tag, provider + " enabled by user");
+        }
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+            //Log.Debug(tag, provider + " availability has changed to " + status.ToString());
+        }
+
+        public void OnMapReady(GoogleMap map)
+        {
+            _map = map;
+            SetupMapIfNeeded();
+        }
+
     }
 }
 
--- AndroidMapGame/Resources/Resource.Designer.cs
+++ AndroidMapGame/Resources/Resource.Designer.cs
@@ -26,10 +26,98 @@ namespace AndroidMapGame
 		
 		public static void UpdateIdValues()
 		{
+			global::BranchXamarinSDK.Droid.Resource.String.library_name = global::AndroidMapGame.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
 		{
+			
+			// aapt resource value: 0x7f010010
+			public const int ambientEnabled = 2130771984;
+			
+			// aapt resource value: 0x7f01001a
+			public const int buttonSize = 2130771994;
+			
+			// aapt resource value: 0x7f010001
+			public const int cameraBearing = 2130771969;
+			
+			// aapt resource value: 0x7f010012
+			public const int cameraMaxZoomPreference = 2130771986;
+			
+			// aapt resource value: 0x7f010011
+			public const int cameraMinZoomPreference = 2130771985;
+			
+			// aapt resource value: 0x7f010002
+			public const int cameraTargetLat = 2130771970;
+			
+			// aapt resource value: 0x7f010003
+			public const int cameraTargetLng = 2130771971;
+			
+			// aapt resource value: 0x7f010004
+			public const int cameraTilt = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int cameraZoom = 2130771973;
+			
+			// aapt resource value: 0x7f010019
+			public const int circleCrop = 2130771993;
+			
+			// aapt resource value: 0x7f01001b
+			public const int colorScheme = 2130771995;
+			
+			// aapt resource value: 0x7f010018
+			public const int imageAspectRatio = 2130771992;
+			
+			// aapt resource value: 0x7f010017
+			public const int imageAspectRatioAdjust = 2130771991;
+			
+			// aapt resource value: 0x7f010015
+			public const int latLngBoundsNorthEastLatitude = 2130771989;
+			
+			// aapt resource value: 0x7f010016
+			public const int latLngBoundsNorthEastLongitude = 2130771990;
+			
+			// aapt resource value: 0x7f010013
+			public const int latLngBoundsSouthWestLatitude = 2130771987;
+			
+			// aapt resource value: 0x7f010014
+			public const int latLngBoundsSouthWestLongitude = 2130771988;
+			
+			// aapt resource value: 0x7f010006
+			public const int liteMode = 2130771974;
+			
+			// aapt resource value: 0x7f010000
+			public const int mapType = 2130771968;
+			
+			// aapt resource value: 0x7f01001c
+			public const int scopeUris = 2130771996;
+			
+			// aapt resource value: 0x7f010007
+			public const int uiCompass = 2130771975;
+			
+			// aapt resource value: 0x7f01000f
+			public const int uiMapToolbar = 2130771983;
+			
+			// aapt resource value: 0x7f010008
+			public const int uiRotateGestures = 2130771976;
+			
+			// aapt resource value: 0x7f010009
+			public const int uiScrollGestures = 2130771977;
+			
+			// aapt resource value: 0x7f01000a
+			public const int uiTiltGestures = 2130771978;
+			
+			// aapt resource value: 0x7f01000b
+			public const int uiZoomControls = 2130771979;
+			
+			// aapt resource value: 0x7f01000c
+			public const int uiZoomGestures = 2130771980;
+			
+			// aapt resource value: 0x7f01000d
+			public const int useViewLifecycle = 2130771981;
+			
+			// aapt resource value: 0x7f01000e
+			public const int zOrderOnTop = 2130771982;
 			
 			static Attribute()
 			{
@@ -41,11 +129,204 @@ namespace AndroidMapGame
 			}
 		}
 		
-		public partial class Layout
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040008
+			public const int common_google_signin_btn_text_dark = 2130968584;
+			
+			// aapt resource value: 0x7f040000
+			public const int common_google_signin_btn_text_dark_default = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int common_google_signin_btn_text_dark_disabled = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int common_google_signin_btn_text_dark_focused = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int common_google_signin_btn_text_dark_pressed = 2130968579;
+			
+			// aapt resource value: 0x7f040009
+			public const int common_google_signin_btn_text_light = 2130968585;
+			
+			// aapt resource value: 0x7f040004
+			public const int common_google_signin_btn_text_light_default = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int common_google_signin_btn_text_light_disabled = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int common_google_signin_btn_text_light_focused = 2130968582;
+			
+			// aapt resource value: 0x7f040007
+			public const int common_google_signin_btn_text_light_pressed = 2130968583;
+			
+			// aapt resource value: 0x7f04000a
+			public const int common_google_signin_btn_tint = 2130968586;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int common_full_open_on_phone = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int common_google_signin_btn_icon_dark = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int common_google_signin_btn_icon_dark_focused = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int common_google_signin_btn_icon_dark_normal = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int common_google_signin_btn_icon_dark_normal_background = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int common_google_signin_btn_icon_disabled = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int common_google_signin_btn_icon_light = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int common_google_signin_btn_icon_light_focused = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int common_google_signin_btn_icon_light_normal = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int common_google_signin_btn_icon_light_normal_background = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int common_google_signin_btn_text_dark = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int common_google_signin_btn_text_dark_focused = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int common_google_signin_btn_text_dark_normal = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int common_google_signin_btn_text_dark_normal_background = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int common_google_signin_btn_text_disabled = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int common_google_signin_btn_text_light = 2130837519;
+			
+			// aapt resource value: 0x7f020010
+			public const int common_google_signin_btn_text_light_focused = 2130837520;
+			
+			// aapt resource value: 0x7f020011
+			public const int common_google_signin_btn_text_light_normal = 2130837521;
+			
+			// aapt resource value: 0x7f020012
+			public const int common_google_signin_btn_text_light_normal_background = 2130837522;
+			
+			// aapt resource value: 0x7f020013
+			public const int googleg_disabled_color_18 = 2130837523;
+			
+			// aapt resource value: 0x7f020014
+			public const int googleg_standard_color_18 = 2130837524;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f070005
+			public const int adjust_height = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int adjust_width = 2131165190;
+			
+			// aapt resource value: 0x7f07000a
+			public const int auto = 2131165194;
+			
+			// aapt resource value: 0x7f07000b
+			public const int dark = 2131165195;
+			
+			// aapt resource value: 0x7f070000
+			public const int hybrid = 2131165184;
+			
+			// aapt resource value: 0x7f070007
+			public const int icon_only = 2131165191;
+			
+			// aapt resource value: 0x7f07000c
+			public const int light = 2131165196;
+			
+			// aapt resource value: 0x7f07000d
+			public const int map = 2131165197;
+			
+			// aapt resource value: 0x7f070001
+			public const int none = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int normal = 2131165186;
+			
+			// aapt resource value: 0x7f070003
+			public const int satellite = 2131165187;
+			
+			// aapt resource value: 0x7f070008
+			public const int standard = 2131165192;
+			
+			// aapt resource value: 0x7f070004
+			public const int terrain = 2131165188;
+			
+			// aapt resource value: 0x7f070009
+			public const int wide = 2131165193;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Integer
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int google_play_services_version = 2131099648;
+			
+			static Integer()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Integer()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
 			
 			static Layout()
 			{
@@ -60,8 +341,65 @@ namespace AndroidMapGame
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f050012
+			public const int app_name = 2131034130;
+			
+			// aapt resource value: 0x7f050000
+			public const int common_google_play_services_enable_button = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int common_google_play_services_enable_text = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int common_google_play_services_enable_title = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int common_google_play_services_install_button = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int common_google_play_services_install_text = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int common_google_play_services_install_title = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int common_google_play_services_notification_ticker = 2131034118;
+			
+			// aapt resource value: 0x7f050010
+			public const int common_google_play_services_unknown_issue = 2131034128;
+			
+			// aapt resource value: 0x7f050007
+			public const int common_google_play_services_unsupported_text = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int common_google_play_services_update_button = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int common_google_play_services_update_text = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int common_google_play_services_update_title = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int common_google_play_services_updating_text = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int common_google_play_services_wear_update_text = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int common_open_on_phone = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int common_signin_button_text = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int common_signin_button_text_long = 2131034127;
+			
+			// aapt resource value: 0x7f050011
+			public const int library_name = 2131034129;
+			
+			// aapt resource value: 0x7f050013
+			public const int map_api_key = 2131034131;
 			
 			static String()
 			{
@@ -69,6 +407,141 @@ namespace AndroidMapGame
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] LoadingImageView = new int[] {
+					2130771991,
+					2130771992,
+					2130771993};
+			
+			// aapt resource value: 2
+			public const int LoadingImageView_circleCrop = 2;
+			
+			// aapt resource value: 1
+			public const int LoadingImageView_imageAspectRatio = 1;
+			
+			// aapt resource value: 0
+			public const int LoadingImageView_imageAspectRatioAdjust = 0;
+			
+			public static int[] MapAttrs = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978,
+					2130771979,
+					2130771980,
+					2130771981,
+					2130771982,
+					2130771983,
+					2130771984,
+					2130771985,
+					2130771986,
+					2130771987,
+					2130771988,
+					2130771989,
+					2130771990};
+			
+			// aapt resource value: 16
+			public const int MapAttrs_ambientEnabled = 16;
+			
+			// aapt resource value: 1
+			public const int MapAttrs_cameraBearing = 1;
+			
+			// aapt resource value: 18
+			public const int MapAttrs_cameraMaxZoomPreference = 18;
+			
+			// aapt resource value: 17
+			public const int MapAttrs_cameraMinZoomPreference = 17;
+			
+			// aapt resource value: 2
+			public const int MapAttrs_cameraTargetLat = 2;
+			
+			// aapt resource value: 3
+			public const int MapAttrs_cameraTargetLng = 3;
+			
+			// aapt resource value: 4
+			public const int MapAttrs_cameraTilt = 4;
+			
+			// aapt resource value: 5
+			public const int MapAttrs_cameraZoom = 5;
+			
+			// aapt resource value: 21
+			public const int MapAttrs_latLngBoundsNorthEastLatitude = 21;
+			
+			// aapt resource value: 22
+			public const int MapAttrs_latLngBoundsNorthEastLongitude = 22;
+			
+			// aapt resource value: 19
+			public const int MapAttrs_latLngBoundsSouthWestLatitude = 19;
+			
+			// aapt resource value: 20
+			public const int MapAttrs_latLngBoundsSouthWestLongitude = 20;
+			
+			// aapt resource value: 6
+			public const int MapAttrs_liteMode = 6;
+			
+			// aapt resource value: 0
+			public const int MapAttrs_mapType = 0;
+			
+			// aapt resource value: 7
+			public const int MapAttrs_uiCompass = 7;
+			
+			// aapt resource value: 15
+			public const int MapAttrs_uiMapToolbar = 15;
+			
+			// aapt resource value: 8
+			public const int MapAttrs_uiRotateGestures = 8;
+			
+			// aapt resource value: 9
+			public const int MapAttrs_uiScrollGestures = 9;
+			
+			// aapt resource value: 10
+			public const int MapAttrs_uiTiltGestures = 10;
+			
+			// aapt resource value: 11
+			public const int MapAttrs_uiZoomControls = 11;
+			
+			// aapt resource value: 12
+			public const int MapAttrs_uiZoomGestures = 12;
+			
+			// aapt resource value: 13
+			public const int MapAttrs_useViewLifecycle = 13;
+			
+			// aapt resource value: 14
+			public const int MapAttrs_zOrderOnTop = 14;
+			
+			public static int[] SignInButton = new int[] {
+					2130771994,
+					2130771995,
+					2130771996};
+			
+			// aapt resource value: 0
+			public const int SignInButton_buttonSize = 0;
+			
+			// aapt resource value: 1
+			public const int SignInButton_colorScheme = 1;
+			
+			// aapt resource value: 2
+			public const int SignInButton_scopeUris = 2;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
